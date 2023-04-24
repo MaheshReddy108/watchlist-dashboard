07553bd14694d3d787d5b0f22177d71a237a23d0
MaheshReddy108 <maheshreddy.konatham@sjsu.edu>
2023-04-24 15:47:25 -0700

    added private IP

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,6 @@ app.use(cors({ origin: `http://3.95.239.250:3000` }));
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', `http://3.95.239.250:3000`);
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   res.setHeader('Cache-Control', 'no-cache');
