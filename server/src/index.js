a83bdb94f6a7dbe21e08f4b21bac09f2febce324
MaheshReddy108 <maheshreddy.konatham@sjsu.edu>
2023-04-24 14:45:55 -0700

    added preflightContinue

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  preflightContinue: true
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -19,4 +21,4 @@ app.use('/',watchlistRouter );
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
-  });
+});
