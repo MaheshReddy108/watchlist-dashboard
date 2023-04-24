a993a781b1a354e15b3e6ec2afbbf2a80d82307c
MaheshReddy108 <maheshreddy.konatham@sjsu.edu>
2023-04-24 02:28:57 -0700

    added test files

---
+++ client/src/test/QuoteDetails.test.js
@@ -0,0 +1,73 @@
+import React from "react";
+import axios from "axios";
+import { render, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import QuoteDetails from "./QuoteDetails";
+import quoteDetailsStore from "../store/QuoteDetailsStore";
+
+jest.mock("axios");
+
+describe("QuoteDetails component", () => {
+  beforeEach(() => {
+    quoteDetailsStore.setQuoteDetails({});
+  });
+
+  test("renders the 'Please select a symbol' message when no symbol is selected", () => {
+    const { getByText } = render(<QuoteDetails selectedSymbol={null} />);
+    expect(getByText("Please select a symbol from the watchlist.")).toBeInTheDocument();
+  });
+
+  test("renders the 'Quote Details' panel with correct header when symbol is selected", async () => {
+    const mockResponse = {
+      data: {
+        bid: 100,
+        ask: 110,
+        bidSize: 50,
+        askSize: 60,
+        volume: 500,
+        quoteDetails: "Some details"
+      }
+    };
+    axios.get.mockResolvedValue(mockResponse);
+
+    const { getByText } = render(<QuoteDetails selectedSymbol="AAPL" />);
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+    const headerText = getByText("Quote Details for AAPL");
+    expect(headerText).toBeInTheDocument();
+    expect(headerText).toHaveStyle("color: white");
+  });
+
+  test("displays the quote details when symbol is selected and data is fetched successfully", async () => {
+    const mockResponse = {
+      data: {
+        bid: 100,
+        ask: 110,
+        bidSize: 50,
+        askSize: 60,
+        volume: 500,
+        quoteDetails: "Some details"
+      }
+    };
+    axios.get.mockResolvedValue(mockResponse);
+
+    const { getByText } = render(<QuoteDetails selectedSymbol="AAPL" />);
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+    expect(getByText("Bid: 100")).toBeInTheDocument();
+    expect(getByText("Ask: 110")).toBeInTheDocument();
+    expect(getByText("Bid Size: 50")).toBeInTheDocument();
+    expect(getByText("Ask Size: 60")).toBeInTheDocument();
+    expect(getByText("Volume: 500")).toBeInTheDocument();
+    expect(getByText("Quote Details: Some details")).toBeInTheDocument();
+  });
+
+  test("displays error message when symbol is selected but data fetching fails", async () => {
+    axios.get.mockRejectedValue(new Error("Error fetching data"));
+
+    const { getByText } = render(<QuoteDetails selectedSymbol="AAPL" />);
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+
+    expect(getByText("Error fetching data")).toBeInTheDocument();
+  });
+});
--- client/src/test/WatchList.test.js
+++ client/src/test/WatchList.test.js
@@ -1,25 +1,87 @@
 import React from "react";
-const { shallow } = require("enzyme");
-const { expect } = require("chai");
-const sinon = require("sinon");
-const Watchlist = require("../components/WatchList.js").default;
+import { render, screen, fireEvent } from "@testing-library/react";
+import Watchlist from "./Watchlist";
 
-describe("<Watchlist />", () => {
-  it("should render the correct header", () => {
-    const wrapper = shallow(<Watchlist />);
-    expect(wrapper.find(".watchlist-header").text()).to.equal("Watchlist Dashboard");
+describe("Watchlist component", () => {
+  test("renders Watchlist Dashboard header", () => {
+    render(<Watchlist />);
+    const headerElement = screen.getByText(/Watchlist Dashboard/i);
+    expect(headerElement).toBeInTheDocument();
   });
 
-  it("should call handleAdd when the 'Add' button is clicked", () => {
-    const handleAddSpy = sinon.spy();
-    const wrapper = shallow(<Watchlist handleAdd={handleAddSpy} />);
-    wrapper.find(".watchlist-add-btn").simulate("click");
-    expect(handleAddSpy.calledOnce).to.equal(true);
+  test("renders symbols table", () => {
+    render(<Watchlist />);
+    const tableElement = screen.getByRole("table");
+    expect(tableElement).toBeInTheDocument();
   });
 
-  it("should update the input value when the input changes", () => {
-    const wrapper = shallow(<Watchlist />);
-    wrapper.find(".watchlist-input").simulate("change", { target: { value: "AAPL" } });
-    expect(wrapper.find(".watchlist-input").prop("value")).to.equal("AAPL");
+  test("renders add symbol input field", () => {
+    render(<Watchlist />);
+    const inputElement = screen.getByPlaceholderText(/Enter Symbol/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test("renders available symbols", () => {
+    render(<Watchlist />);
+    const availableSymbolsElement = screen.getByText(/Available Symbols:/i);
+    expect(availableSymbolsElement).toBeInTheDocument();
+  });
+
+  test("clicking on a symbol in the table opens the quote details", () => {
+    render(<Watchlist />);
+    const tableElement = screen.getByRole("table");
+    const symbolLinkElement = tableElement.querySelector("a");
+    fireEvent.click(symbolLinkElement);
+    const quoteDetailsElement = screen.getByText(/Quote Details:/i);
+    expect(quoteDetailsElement).toBeInTheDocument();
+  });
+
+  test("adding a symbol updates the table", () => {
+    render(<Watchlist />);
+    const inputElement = screen.getByPlaceholderText(/Enter Symbol/i);
+    const addButtonElement = screen.getByText(/Add/i);
+    const symbolToAdd = "AAPL";
+    fireEvent.change(inputElement, { target: { value: symbolToAdd } });
+    fireEvent.click(addButtonElement);
+    const symbolLinkElement = screen.getByText(symbolToAdd);
+    expect(symbolLinkElement).toBeInTheDocument();
+  });
+
+  test("calls setSelectedUser when user is selected", () => {
+    render(<Watchlist />);
+    const select = screen.getByRole("combobox");
+    fireEvent.change(select, { target: { value: "user2" } });
+    expect(select.value).toBe("user2");
+    expect(watchlistStore.setSelectedUser).toHaveBeenCalledWith("user2");
+  });
+
+  test("clicking the update button for a symbol calls handleUpdate", () => {
+    const mockHandleUpdate = jest.fn();
+    watchlistStore.handleUpdate = mockHandleUpdate;
+
+    const { getByRole } = render(<Watchlist />);
+    const symbol = "AAPL";
+    const updateButton = getByRole("button", {
+      name: /update/i,
+      exact: false,
+      hidden: false,
+      label: new RegExp(symbol, "i")
+    });
+
+    fireEvent.click(updateButton);
+
+    expect(mockHandleUpdate).toHaveBeenCalledTimes(1);
+    expect(mockHandleUpdate).toHaveBeenCalledWith(symbol);
+  });
+
+  test("deleting a symbol updates the table", () => {
+    render(<Watchlist />);
+    const tableElement = screen.getByRole("table");
+    const deleteButtonElement = tableElement.querySelector(
+      "button[aria-label='delete']"
+    );
+    fireEvent.click(deleteButtonElement);
+    const deletedSymbolLinkElement = screen.queryByText(/AAPL/i);
+    expect(deletedSymbolLinkElement).not.toBeInTheDocument();
   });
 });
