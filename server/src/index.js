84179193a86e7c36535f75174b1ebf72a1a6a4be
MaheshReddy108 <maheshreddy.konatham@sjsu.edu>
2023-04-24 15:50:54 -0700

    added private IP

--- server/src/index.js
+++ server/src/index.js
@@ -15,21 +15,21 @@ const corsOptions = {
 
 
 
-app.use(cors({ origin: `http://172.31.21.15:3000` }));
+// app.use(cors({ origin: `http://172.31.21.15:3000` }));
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', `http://172.31.21.15:3000`);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', `http://172.31.21.15:3000`);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+// res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//   res.setHeader('Cache-Control', 'no-cache');
+//   next();
+// });
 
-// Middleware
-// app.use(cors({
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-// }));
+Middleware
+app.use(cors({
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
