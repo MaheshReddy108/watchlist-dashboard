1931f3b5fc6a9a24fd91540da3b4aab756f7d9bc
MaheshReddy108 <maheshreddy.konatham@sjsu.edu>
2023-04-24 17:10:45 -0700

    added promises

--- client/src/store/WatchListStore.js
+++ client/src/store/WatchListStore.js
@@ -31,128 +31,134 @@ class WatchlistStore {
   }
 
   fetchData = () => {
-    return axios.get(`http://${host}:3001/watchlist/${this.selectedUser}`, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(response => {
-      this.symbols = response.data;
-    })
-    .catch(error => {
-      this.showSweetAlert(error.response.data.error);
-    });
+    return axios
+      .get(`http://${host}:3001/watchlist/${this.selectedUser}`, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        this.symbols = response.data;
+      })
+      .catch(error => {
+        this.showSweetAlert(error.response.data.error);
+      });
   };
-  
+
   setSelectedUser = user => {
     this.selectedUser = user;
     return Promise.resolve();
   };
-  
+
   setInputValue = value => {
     this.inputValue = value;
     return Promise.resolve();
   };
-  
+
   fetchSymbolsInfo = () => {
-    return axios.get(`http://${host}:3001/allsymbols`, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(response => {
-      this.symbolsInfo = response.data;
-    })
-    .catch(error => {
-      console.log(error);
-      this.showSweetAlert(error.response.data.error);
-    });
+    return axios
+      .get(`http://${host}:3001/allsymbols`, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        this.symbolsInfo = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+        this.showSweetAlert(error.response.data.error);
+      });
   };
-  
+
   handleDelete = symbol => {
-    return axios.delete(`http://${host}:3001/watchlist/${this.selectedUser}/remove`, {
-      headers: {
-        "Content-Type": "application/json"
-      },
-      data: { symbol }
-    })
-    .then(() => {
-      return this.fetchData();
-    })
-    .catch(error => {
-      this.showSweetAlert(error.response.data.error);
-    });
+    return axios
+      .delete(`http://${host}:3001/watchlist/${this.selectedUser}/remove`, {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        data: { symbol }
+      })
+      .then(() => {
+        return this.fetchData();
+      })
+      .catch(error => {
+        this.showSweetAlert(error.response.data.error);
+      });
   };
-  
+
   handleAdd = () => {
     if (this.inputValue.trim() === "") {
       this.showSweetAlert("Enter a Valid Symbol");
       return Promise.resolve();
     }
-    return axios.post(`http://${host}:3001/watchlist/${this.selectedUser}/add`, {
-      symbol: this.inputValue.toUpperCase()
-    }, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(() => {
-      setTimeout(() => {
+    return axios
+      .post(
+        `http://${host}:3001/watchlist/${this.selectedUser}/add`,
+        {
+          symbol: this.inputValue.toUpperCase()
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      )
+      .then(() => {
         this.fetchData();
-      }, 10000);
-      this.showSweetAlert(
-        `Symbol "${this.inputValue.toUpperCase()}" has been added successfully.`,
-        "success",
-        "Success!"
-      );
-      this.inputValue = "";
-    })
-    .catch(error => {
-      this.showSweetAlert(error.response.data.error);
-      this.inputValue = "";
-    });
+        this.showSweetAlert(
+          `Symbol "${this.inputValue.toUpperCase()}" has been added successfully.`,
+          "success",
+          "Success!"
+        );
+        this.inputValue = "";
+      })
+      .catch(error => {
+        this.showSweetAlert(error.response.data.error);
+        this.inputValue = "";
+      });
   };
-  
+
   handleRowClick = symbol => {
     this.isRowClicked = true;
     this.selectedSymbol = symbol;
   };
-  
+
   handleUpdate = symbol => {
-    return axios.get(`http://${host}:3001/symbol/${symbol}`)
-    .then(response => {
-      const updatedData = response.data;
-      const newData = Object.keys(this.symbols).map(key => {
-        if (key === symbol) {
-          const updatedRow = {
-            ...this.symbols[key],
-            ...updatedData
-          };
-          return {
-            key,
-            symbol: key,
-            ...updatedRow
-          };
-        } else {
-          return {
-            key,
-            symbol: key,
-            ...this.symbols[key]
-          };
-        }
+    return axios
+      .get(`http://${host}:3001/symbol/${symbol}`)
+      .then(response => {
+        const updatedData = response.data;
+        const newData = Object.keys(this.symbols).map(key => {
+          if (key === symbol) {
+            const updatedRow = {
+              ...this.symbols[key],
+              ...updatedData
+            };
+            return {
+              key,
+              symbol: key,
+              ...updatedRow
+            };
+          } else {
+            return {
+              key,
+              symbol: key,
+              ...this.symbols[key]
+            };
+          }
+        });
+        this.showSweetAlert(
+          `Symbol "${symbol}" has been updated successfully.`,
+          "success",
+          "Success!"
+        );
+        this.symbols = newData;
+      })
+      .catch(error => {
+        this.showSweetAlert(error.response.data.error);
       });
-      this.showSweetAlert(
-        `Symbol "${symbol}" has been updated successfully.`,
-        "success",
-        "Success!"
-      );
-      this.symbols = newData;
-    })
-    .catch(error => {
-      this.showSweetAlert(error.response.data.error);
-    });
   };
-  
 
   showSweetAlert = (message, icon = "error", title = "Oops...") => {
     Swal.fire({
